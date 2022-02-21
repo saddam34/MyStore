46ec71fc92f12c1fa534b318c00ecc8614e1dc78
saddam34 <54130760+saddam34@users.noreply.github.com>
2022-02-21 12:18:58 +0530

    Update RouteConfig.cs

--- MyStore/App_Start/RouteConfig.cs
+++ MyStore/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace MyStore
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Product", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Category", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
